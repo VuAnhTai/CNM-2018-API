9dc864008d020093e6e0dd29d2704732d6386ec5
vuanhtai <vuanhtai1997@gmail.com>
2018-11-13 21:21:29 +0700

    token basic and google api

---
+++ App2/index.app.js
@@ -0,0 +1,136 @@
+window.onload = function() {
+    vm.setupSSE();
+    vm.initMap();
+    // loadCategories();
+};
+
+
+var vm = new Vue({
+    el: '#container',
+    data: {
+        userName: '',
+        password:'',
+        loginVisible:true,
+        requestsVisible:false,
+        requests: [],
+        token:"",
+        refToken:"",
+        geocoder : {lat: 10.7623314, lng: 106.6820053}
+    },
+    methods: {
+        login: function() {
+            var self = this;
+            axios.post('http://localhost:3000/app2/login', {
+                userName: self.userName,
+                password: self.password,
+            })
+                .then(function (response) {
+                    self.token=response.data.access_token;
+                    self.refToken=response.data.refresh_token;
+                })
+                .catch(function (error) {
+                    alert(error);
+                }).then(function () {
+                    self.getAllRequest();
+            })
+
+        },
+        getAllRequest:function(){
+            var self = this;
+            axios.get('http://localhost:3000/api/request/getAll',{ headers: { token: self.token } })
+                .then(function (response) {
+                    console.log('vo');
+                    self.requests=response.data;
+                    self.requestsVisible=true;
+                    self.loginVisible=false;
+                    $('#tableDH').DataTable().destroy();
+                })
+                .catch(function (error) {
+                    if (error.response.status===401){
+                        self.refreshToken();
+                        return;
+                    }
+
+                }).then(function () {
+
+                $('#tableDH').DataTable();
+            });
+        },
+        refreshToken:function () {
+            var self = this;
+            console.log("ref");
+            axios.post('http://localhost:3000/api/users/refreshToken', {
+                refToken: self.refToken,
+            })
+                .then(function (response) {
+                    self.token=response.data.access_token;
+                    self.getAllRequest();
+                })
+                .catch(function (error) {
+                    if (error.response.status===401){
+                        self.loginVisible=true;
+                        self.requestsVisible=false;
+                    }
+                }).then(function () {
+
+            })
+        },
+        setupSSE : function() {
+            var self = this;
+            if (typeof(EventSource) === 'undefined') {
+                console.log('not support');
+                return;
+            }
+
+            var src = new EventSource('http://localhost:3000/requestAddedEvent');
+
+            src.onerror = function(e) {
+                console.log('error: ' + e);
+            }
+
+            src.addEventListener('REQUEST_ADDED', function(e) {
+                var data = JSON.parse(e.data);
+                self.requests.push(data);
+                self.refDataTable();
+
+            }, false);
+        },
+        refDataTable:function () {
+            new Promise(function (resolve,reject) {
+                $('#tableDH').DataTable().destroy();
+                resolve();
+            }).then(function () {
+                $('#tableDH').DataTable();
+            })
+        },
+        initMap:function() {
+            var self=this;
+            var address = "18/13 Trần Văn Thành";
+            var geocoder = new google.maps.Geocoder();
+            geocoder.geocode( { 'address': address}, function(results, status) {
+
+                if (status == google.maps.GeocoderStatus.OK) {
+                    self.geocoder.lat = results[0].geometry.location.lat();
+                    self.geocoder.lng = results[0].geometry.location.lng();
+
+                    var map = new google.maps.Map(
+                        document.getElementById('map'), {zoom: 18, center: self.geocoder});
+                    var marker = new google.maps.Marker({position: self.geocoder, map: map});
+
+
+
+                }else {
+                    alert("false");
+                }
+            });
+
+
+
+
+}
+
+    }
+
+});
+
+
---
+++ server/repos/userRepo.js
@@ -0,0 +1,33 @@
+var md5 = require('crypto-js/md5');
+
+var db = require('../fn/mysql-db');
+
+exports.add = userEntity => {
+    var md5_pwd = md5(userEntity.password);
+    var sql = `insert into users(username, password, permission) values('${userEntity.username}', '${md5_pwd}', ${userEntity.permission})`;
+    return db.insert(sql);
+}
+exports.addRequest = userEntity => {
+    var sql = `insert into requests(name, phone, address, note, status) values('${userEntity.name}', '${userEntity.phone}', '${userEntity.address}', '${userEntity.note}','${userEntity.status}')`;
+    return db.insert(sql);
+}
+exports.login = loginEntity => {
+    var md5_pwd = md5(loginEntity.password);
+	var sql = `select * from users where username = '${loginEntity.userName}' and password = '${md5_pwd}'`;
+    return db.load(sql);
+};
+exports.checkUserName=userName=>{
+    var sql = `select * from users where username = '${userName}'`;
+    return db.load(sql);
+}
+exports.getUserInfo=userId=>{
+    var sql = `select * from users where ID = '${userId}'`;
+    return db.load(sql);
+}
+
+
+
+
+
+
+
