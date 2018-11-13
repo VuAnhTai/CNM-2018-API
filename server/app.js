20f35b99a306caf58bed533f7ef8ad00fdbd15e8
vuanhtai <vuanhtai1997@gmail.com>
2018-11-13 21:36:45 +0700

    format code

--- server/app.js
+++ server/app.js
@@ -1,41 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var productCtrl = require('./apiControllers/productControllers');
+var userCtrl = require('./apiControllers/userControllers');
+var requestCtrl = require('./apiControllers/requestController');
+var app1Ctrl = require('./apiControllers/app1Controller');
+var app2Ctrl = require('./apiControllers/app2Controller');
+var events = require('./events');
+var verifyAccessToken = require('./repos/authRepo').verifyAccessToken;
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', (req, res) => {
+    res.json({
+        msg: 'hello from nodejs express api'
+    })
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+// app.use('/api/products/', productCtrl);
+app.use('/api/users/', userCtrl);
+app.use('/api/request', verifyAccessToken, requestCtrl);
+app.use('/app1/', app1Ctrl);
+app.use('/app2/', app2Ctrl);
+app.get('/requestAddedEvent', events.subscribeRequestAdded);
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`QLBH API is running on port ${port}`);
+})
